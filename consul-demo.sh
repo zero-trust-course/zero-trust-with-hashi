b4f575194c5be6d28611f49220ab72fc759fdd6c
andel7 <lev.andelman@gmail.com>
2022-01-16 10:48:40 +0200

    Update consul-demo.sh

--- consul-demo.sh
+++ consul-demo.sh
@@ -36,6 +36,7 @@ pe 'echo $DATACENTER && \
 
 cat > config.yaml << EOF
 global:
+  logLevel: "debug"
   name: consul
   enabled: false
   datacenter: ${DATACENTER}
@@ -63,19 +64,144 @@ client:
   enabled: true
   join: ${RETRY_JOIN}
 connectInject:
-  enabled: false
+  enabled: true
+  default: true
+  transparentProxy:
+    defaultEnabled: true
 controller:
-  enabled: false
+  enabled: true
 ingressGateways:
   enabled: false
 syncCatalog:
-  enabled: true
+  enabled: false
 EOF
 
 pe 'cat config.yaml'
-pe 'helm install --wait consul -f config.yaml hashicorp/consul --version "0.32.1" --set global.image=hashicorp/consul-enterprise:1.10.1-ent'
+pe 'helm install --wait consul -f config.yaml hashicorp/consul --version "0.39.0" --set global.image=hashicorp/consul-enterprise:1.10.6-ent'
 
 pe 'kubectl get pods'
 
 p "Open $(terraform output consul_public_endpoint) and use ${CONSUL_HTTP_TOKEN} token to login"
 pe 'kubectl get svc'
+
+
+pe 'cd /tmp'
+pe 'git clone https://github.com/GoogleCloudPlatform/microservices-demo.git'
+pe 'diff  /02-hcp/kubernetes-manifests.yaml /tmp/microservices-demo/release/kubernetes-manifests.yaml'
+pe 'cp  /02-hcp/kubernetes-manifests.yaml /tmp/microservices-demo/release/kubernetes-manifests.yaml'
+
+pe 'for i in checkoutservice adservice cartservice currencyservice emailservice frontend loadgenerator paymentservice productcatalogservice recommendationservice redis-cart shippingservice; do kubectl create sa ${i};done'
+
+pe 'cd microservices-demo'
+
+#add service account matching the service in consul
+kubectl apply -f ./release/kubernetes-manifests.yaml
+
+pe 'helm repo add traefik https://helm.traefik.io/traefik'
+
+pe 'helm repo update'
+
+export KUBERNETES_SVC_IP=$(kubectl get svc kubernetes -o=jsonpath='{.spec.clusterIP}')
+cat <<EOF > traefik_values.yaml
+deployment:
+   podAnnotations:
+      consul.hashicorp.com/connect-inject: "true"
+      consul.hashicorp.com/connect-service: "traefik"
+      consul.hashicorp.com/transparent-proxy: "true"
+      consul.hashicorp.com/transparent-proxy-overwrite-probes: "true"
+      consul.hashicorp.com/transparent-proxy-exclude-inbound-ports: "9000,8000,8443"
+      consul.hashicorp.com/transparent-proxy-exclude-outbound-ports: "443"
+      consul.hashicorp.com/transparent-proxy-exclude-outbound-cidrs: "${KUBERNETES_SVC_IP}/32"
+
+logs:
+   general:
+      level: DEBUG
+EOF
+pe 'cat traefik_values.yaml'
+
+pe 'helm install -n default traefik traefik/traefik -f traefik_values.yaml'
+
+cat <<EOF > consul.yaml
+apiVersion: consul.hashicorp.com/v1alpha1
+kind: ServiceDefaults
+metadata:
+  name: traefik
+spec:
+  protocol: http
+EOF
+
+pe 'cat consul.yaml'
+pe 'kubectl apply -f consul.yaml'
+pe 'kubectl get svc traefik'
+export TRAEFIK_SVC=$(kubectl get services traefik -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+pe 'echo $TRAEFIK_SVC'
+cat <<EOF > openssl.cnf
+[req]
+distinguished_name = req_distinguished_name
+req_extensions = v3_req
+prompt = no
+[req_distinguished_name]
+C = IL
+ST = TA
+L = Tel Aviv
+O = Terasky
+OU = IT
+CN = *.elb.amazonaws.com
+[v3_req]
+keyUsage = keyEncipherment, dataEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = ${TRAEFIK_SVC}
+EOF
+pe 'cat openssl.cnf'
+pe 'openssl req -config openssl.cnf -newkey rsa:4096  -x509 -sha256 -days 3650 -nodes -out server.crt -keyout server.key'
+pe 'kubectl create secret generic traefik --from-file=tls.crt=./server.crt --from-file=tls.key=./server.key'
+
+cat <<EOF > ingress.yaml
+apiVersion: traefik.containo.us/v1alpha1
+kind: IngressRoute
+metadata:
+  name: frontend
+spec:
+  entryPoints:
+    - websecure
+  routes:
+  - match: PathPrefix(\`/\`)
+    kind: Rule
+    services:
+    - name: frontend
+      port: 80
+      passHostHeader: false
+  tls:
+    domains:
+    - main: ${TRAEFIK_SVC}
+---
+apiVersion: traefik.containo.us/v1alpha1
+kind: TLSStore
+metadata:
+  name: default
+  namespace: default
+spec:
+  defaultCertificate:
+    secretName: traefik
+EOF
+
+pe 'cat ingress.yaml'
+pe 'kubectl apply -f ingress.yaml'
+
+cat <<EOF > frontend.yaml
+apiVersion: consul.hashicorp.com/v1alpha1
+kind: ServiceDefaults
+metadata:
+  name: frontend
+spec:
+  protocol: http
+  transparentProxy:
+    dialedDirectly: true
+EOF
+pe 'cat frontend.yaml'
+pe 'kubectl apply -f frontend.yaml'
+pe 'cat /02-hcp/intentions.yaml'
+pe 'kubectl apply -f /02-hcp/intentions.yaml'
+p 'Browse to https://${TRAEFIK_SVC}'
